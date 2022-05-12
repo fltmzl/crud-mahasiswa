e387e5f126d4d582d262112bb658e7b5755cfa2e
Rizzle <fltmzl2810@gmail.com>
2022-05-12 09:30:47 +0700

    initial commit / membuat tab database

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./**/*.{html,js,php}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#4ade80",
+        secondary: "#A3DCAA",
+      },
+      fontFamily: {
+        sans: ["Poppins", "arial", "sans"],
+      },
+    },
+  },
+  plugins: [],
+};
